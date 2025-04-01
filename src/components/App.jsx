7a94452591969cec848f97a36389a664e2eb8a96
Keplin1 <irin.keplin@gmail.com>
2025-04-01 16:17:16 +0100

    post-new-comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from 'react'
+
 import '../App.css'
 import NavigationBar from "./NavigationBar";
 import Home from "./Home";
 import ArticlePage from './ArticlePage'
-
+import { UserProvider } from "../contexts/UserContext";
+import { Container } from "@mui/material";
 
 function App() {
 
   return (
-    <>
-      <NavigationBar />
-      <Routes>
+    <Container>
+      <UserProvider>
+        <NavigationBar />
+        <Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/articles/:article_id" element={<ArticlePage />} />
-      </Routes>
-    </>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles/:article_id" element={<ArticlePage />} />
+        </Routes>
+      </UserProvider >
+    </Container>
   )
 }
 
